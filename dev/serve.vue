c3198e9549e39f2d4fb96bbec916a776d3fe678b
伍俊滨 <540965117@qq.com>
2022-06-24 12:44:35 +0800

    init commit

---
+++ dev/serve.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+import Vue from 'vue';
+// Uncomment import and local "components" registration if library is not registered globally.
+// import { MyComponentsSample } from '@/entry.esm';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  // components: {
+  //  MyComponentsSample,
+  // }
+});
+</script>
+
+<template>
+  <div id="app">
+    <my-components-sample />
+  </div>
+</template>
---
+++ src/lib-components/index.ts
@@ -0,0 +1,2 @@
+/* eslint-disable import/prefer-default-export */
+export { default as MyComponentsSample } from './my-components-sample.vue';
